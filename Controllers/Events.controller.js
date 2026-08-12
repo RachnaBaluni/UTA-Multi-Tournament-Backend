5ac4d88029b67f900ac768ec331b080701bc78d0
Bhargavbaluni <balunibhargav@gmail.com>
2026-08-12 12:27:37 +0530

    fix cors for vercel admin frontend

--- Controllers/Events.controller.js
+++ Controllers/Events.controller.js
@@ -4,7 +4,7 @@ const getAllEvents = async (req, res) => {
   try {
     const { tournamentId } = req.query;
 
-    const events = await eventService.getAllEventsService(tournamentId);
+    const events = await eventService.getAllEventsService();
 
     res.status(200).json({
       success: true,
