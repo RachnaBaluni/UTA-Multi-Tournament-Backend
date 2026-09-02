7644811f70e9a61d5447ae417bdac2330da150bb
Bhargavbaluni <balunibhargav@gmail.com>
2026-09-02 19:15:41 +0530

    Debug venue update

--- Route/Venue.route.js
+++ Route/Venue.route.js
@@ -10,5 +10,11 @@ const router = express.Router();
 
 router.get("/", getAllVenue);
 router.post("/", createVenue);
+router.put("/:id", (req, res, next) => {
+  console.log("========== VENUE PUT ROUTE HIT ==========");
+  console.log("VENUE ID:", req.params.id);
+  console.log("VENUE BODY:", req.body);
 
+  updateVenue(req, res, next);
+});
 module.exports = router;
